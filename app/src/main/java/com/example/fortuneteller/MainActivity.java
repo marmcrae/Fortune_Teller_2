53b9934f59cae6b40a9dd84e85f9e79413aea3a6
marmcrae <mcrae.marquie@gmail.com>
2020-02-01 14:55:41 -1000

    app updates

--- app/src/main/java/com/example/fortuneteller/MainActivity.java
+++ app/src/main/java/com/example/fortuneteller/MainActivity.java
@@ -37,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
         loveArray = new String[]{
                 "You have or will soon find the ONE!",
-                "Give love inwards as well as outwards",
+                "Give love inwards as well as outwards.",
                 "Heartbreak is coming........",
                 "Your outlook for love is bright!",
                 "You have a secret admirer!",
                 "Let them put in the effort.....",
                 "You will meet a new love interest soon!",
-                "Someone is being deceitful",
+                "Someone is being deceitful.",
                 "Lots of love and light surrounds you!",
                 "Outlook not good.....",
                 "Many love blessings are coming your way!",
@@ -53,19 +53,19 @@ public class MainActivity extends AppCompatActivity {
 
         generalArray = new String[]{
                 "You have an unexpected surprise coming your way!",
-                "A Baby is coming",
-                "Karmic lesson is to come. Learn and transcend from the experience",
+                "A Baby is coming.",
+                "Karmic lesson is to come. Learn and transcend from the experience.",
                 "Your spirit guides want you to listen more closely!",
                 "You have a bright future ahead!",
                 "Now is the time for cleansing and renewal",
                 "A blessing is coming your way!",
-                "Someone is being deceitful",
+                "Someone is being deceitful.",
                 "Lots of love and light surrounds you!",
                 "Outlook not good.....",
                 "Many love blessings are coming your way!",
-                "Nobody ever got anywhere by Standing Still. Now is the time",
-                "Get lost in the right direction",
-                "Follow your heart",
+                "Nobody ever got anywhere by Standing Still. Now is the time.",
+                "Get lost in the right direction.",
+                "Follow your heart.",
                 "There are no rules.",
                 "Self Acceptance > Self Improvement",
 
@@ -73,11 +73,11 @@ public class MainActivity extends AppCompatActivity {
 
         moneyArray = new String[]{
                 "You have unexpected abundance coming your way!",
-                "Positive outlook for a business decision you've been pondering",
-                "Clear your energy and money will be drawn to you",
+                "Positive outlook for a business or financial decision you've been pondering.",
+                "Clear your energy and money will be drawn to you.",
                 "New avenues of income are on their way!",
-                "The most fulfilling abundance in life does not come from money",
-                "Now is the time for cleansing and renewal then money will come",
+                "The most fulfilling abundance in life does not come from money.",
+                "Now is the time for cleansing and renewal then money will com.e",
                 "A breakthrough is on the horizon. Keep your faith.",
                 "Be careful of not over spending at this time.",
                 "You will live a very comfortable life."
